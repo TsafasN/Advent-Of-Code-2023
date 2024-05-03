7e7d81ddb62bf9f950fb6215150b9ccabb14283c
ntsafas <ntsafas@gmail.com>
2024-05-03 14:34:44 +0300

    :sparkles: Part 1 solution
    Signed-off-by: ntsafas <ntsafas@gmail.com>

--- day-1/src/bin/part1.rs
+++ day-1/src/bin/part1.rs
@@ -1,13 +1,43 @@
 fn main() {
 
-    let input = include_str!("./input.txt");
-    let output = part1(input);
+    let input_file = include_str!("../../input1.txt");
+    let output = part1(input_file);
 
-    dbg!(output);
+    println!("{:?}", output);
+
+    dbg!(output.unwrap().unwrap());
+    
 }
 
-fn part1(input: &str) -> String {
-    "todo!()".to_string()
+fn part1(input: &str) -> Result<Option<u32>, String> {
+
+    let output = input
+        .lines()
+        .map(|line| {
+            
+            let mut iter = line
+                .chars()
+                .filter_map(|character| { 
+                    character.to_digit(10)
+                });
+
+            let first = iter
+                .next()
+                .expect("Should be a number");
+            
+            let last  = iter
+                .last();
+
+            match last {
+                Some(num) => format!("{first}{num}"),
+                None => format!("{first}{first}"),
+            }.parse::<u32>().expect("should be a valid number")
+
+        })
+        .sum::<u32>();
+
+    Ok(Some(output))
+
 }
 
 #[cfg(test)]
@@ -15,8 +45,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = part1("");
-        assert_eq!(result, "4".to_string());
+    fn test_part1() {
+        let input = "1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet";
+        
+        let result = part1(input);
+
+        assert_eq!(result, Ok(Some(142)));
     }
 }
