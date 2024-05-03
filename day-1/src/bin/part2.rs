148bee23c6bfd016ee5d81f4405dd1e9c68e1399
ntsafas <ntsafas@gmail.com>
2024-05-03 19:42:50 +0300

    :hammer: Removed call to std::iter::from_fn
    Signed-off-by: ntsafas <ntsafas@gmail.com>

--- day-1/src/bin/part2.rs
+++ day-1/src/bin/part2.rs
@@ -22,44 +22,38 @@ fn part1(input: &str) -> Result<Option<u32>, String> {
 
 fn process_line(line: &str) -> u32{
 
-    let mut index = 0;
-    let line_iter = std::iter::from_fn(move || {
-        let reduced_line = &line[index..];
-
-        let result = 
-        if reduced_line.starts_with("one"){
-            Some('1')
-        } else if reduced_line.starts_with("two"){
-            Some('2')
-        } else if reduced_line.starts_with("three"){
-            Some('3')
-        } else if reduced_line.starts_with("four"){
-            Some('4')
-        } else if reduced_line.starts_with("five"){
-            Some('5')
-        } else if reduced_line.starts_with("six"){
-            Some('6')
-        } else if reduced_line.starts_with("seven"){
-            Some('7')
-        } else if reduced_line.starts_with("eight"){
-            Some('8')
-        } else if reduced_line.starts_with("nine"){
-            Some('9')
-        } else if reduced_line.starts_with("zero"){
-            Some('0')
-        } else {
-            let result = reduced_line.chars().next();
-            result
-        };
-        index += 1;
-        result
-    });
+    let mut iter = (0..line.len()).filter_map(
+        |index| {
+            let reduced_line = &line[index..];
+            let result = 
+            if reduced_line.starts_with("one"){
+                '1'
+            } else if reduced_line.starts_with("two"){
+                '2'
+            } else if reduced_line.starts_with("three"){
+                '3'
+            } else if reduced_line.starts_with("four"){
+                '4'
+            } else if reduced_line.starts_with("five"){
+                '5'
+            } else if reduced_line.starts_with("six"){
+                '6'
+            } else if reduced_line.starts_with("seven"){
+                '7'
+            } else if reduced_line.starts_with("eight"){
+                '8'
+            } else if reduced_line.starts_with("nine"){
+                '9'
+            } else if reduced_line.starts_with("zero"){
+                '0'
+            } else {
+                reduced_line.chars().next().unwrap()
+            };
+            
+            result.to_digit(10)
+        }
+    );
     
-    let mut iter = line_iter
-        .filter_map(|character| { 
-            character.to_digit(10)
-        });
-
     let first = iter
         .next()
         .expect("Should be a number");
@@ -70,7 +64,9 @@ fn process_line(line: &str) -> u32{
     match last {
         Some(num) => format!("{first}{num}"),
         None => format!("{first}{first}"),
-    }.parse::<u32>().expect("should be a valid number")
+    }
+    .parse::<u32>()
+    .expect("should be a valid number")
 
 }
 
